ae836bf8008b5d78250403819a72f98a9e6e6247
bugzhu <bugwhen@gmail.com>
2012-12-26 20:21:17 +0800

    a socket sample code

---
+++ sock_server.c
@@ -0,0 +1,84 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+int get_socket_addr(const char *addr, const uint16_t port, struct sockaddr *sa)
+{
+	struct sockaddr_in *sin = (struct sockaddr_in*)sa;
+	sin->sin_family = AF_INET;
+	sin->sin_port = htons(port);
+	if (inet_pton(AF_INET, addr, &sin->sin_addr) > 0) {
+		return 1;
+	}
+	
+	return 0;
+}
+
+void run(const char *addr, const uint16_t port)
+{
+	struct sockaddr_in sin;
+	int sockfd;
+	int clientfd;
+	char buf[1024];
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("call socket failed");
+	}
+
+	if (get_socket_addr(addr, port, (struct sockaddr*)&sin) < 0) {
+		perror("call get_socket_addr failed");
+	}
+
+	if (bind(sockfd, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
+		perror("call bind failed");
+	}
+
+	if (listen(sockfd, 5) < 0) {
+		perror("call listen failed");
+	}
+
+	if ((clientfd = accept(sockfd, NULL, NULL)) < 0) {
+		perror("call accept failed");
+	}
+	
+	while (1) {
+		if (fgets(buf, 1024, stdin) == NULL) {
+			perror("call fgets error");
+			break;
+		}
+		else {
+			size_t sended = send(clientfd, buf, strlen(buf) + 1, 0);
+			if (sended < 0) {
+				perror("call send failed");
+				break;
+			}
+		}
+
+		size_t recved = recv(clientfd, buf, 1024, 0);
+		if (recved < 0) {
+			perror("call recv failed");
+			break;
+		}
+		else {
+			if (fputs(buf, stdout) == EOF) {
+				perror("call fputs failed");
+				break;
+			}
+		}
+	}
+
+	close(clientfd);
+	close(sockfd);
+}
+
+int main()
+{
+	const char *addr = "127.0.0.1";
+	const uint16_t port = 12345;
+	run(addr, port);
+}
